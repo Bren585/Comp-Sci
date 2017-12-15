c711a2f36111a7dbbc0ff141b3560e70262a82db
jane <jr>
2017-12-15 12:09:06 -0800

    imposing pictures

--- Picture Test.py
+++ Picture Test.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 import os.path
 import numpy as np
+import PIL
 
 directory = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(directory, 'loss.png')
-img = plt.imread(filename)
 
-n = int(raw_input('How much loss: '))
+loss = PIL.Image.open(os.path.join(directory, 'loss.png'))
+comp = PIL.Image.open(os.path.join(directory, 'comp.jpg'))
 
-fig, ax = plt.subplots(1,n)
-for x in range(0,n):
-    ax[x].imshow(img, interpolation = 'none')
-    print 'i  |i-i'
-    print ' ii|i _'
-    print ''
+loss = loss.resize((920,570))
+comp.paste(loss,(2120,640))
+
+fig,ax = plt.subplots(1)
+ax.imshow(comp, interpolation = 'none')
 fig.show()
