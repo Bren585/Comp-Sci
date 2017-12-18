9e0acdb132ed295fbccbe2f6e6762c4069c768b6
jane <jr>
2017-12-18 12:08:01 -0800

    GUI improved / completion

--- Duragon.py
+++ Duragon.py
@@ -1,24 +1,70 @@
 import random
 
 E = [
-'Dragon',
-[['HP'],[000],[000]],
-[['MP'],[000],[000]]
+'Evil Dragon',
+['HP','000','000'],
+['MP','000','000']
 ]
 
 name = []
 P = [
-name + 'The Hero',
-[['HP'],[000],[000]],
-[['MP'],[000],[000]]
+str(name) + ' The Hero',
+['HP','000','000'],
+['MP','000','000']
 ]
 
 menu = [
-[[' '],['FIGHT']],
-[[' '],['ITEM']],
-[[' '],['SPELL']],
+['[ ]','FIGHT'],
+['[ ]','ITEM'],
+['[ ]','SPELL'],
 ]
+mSel = []
+
+def dFormat(lst,i):
+    d = lst[i]
+    d = d[0] + ': ' + str(d[1]) + '/' + str(d[2])
+    return d
+
+def mFormat(lst):
+    return str(menu[lst][0]) + str(menu[lst][1])
 
 def display():
-    print E[0]+'             '+P[0]
-    print E[1]
+    print ''
+    print ''
+    print ''
+    print ''
+    print ''
+    print E[0]+'          '+P[0]
+    print dFormat(E,1)+'          '+dFormat(P,1)
+    print dFormat(E,2)+'          '+dFormat(P,2)
+    print ''
+    print 'Command?'
+    print mFormat(0)
+    print mFormat(1)
+    print mFormat(2)
+    
+def PSelect():
+    display()
+    global mSel
+    i = raw_input()
+    while i != '':
+        if mSel == []:
+            if i == 'w':
+                mSel = 0
+            elif i == 's':
+                mSel = 2
+        else:
+            menu[mSel][0] = '[ ]'
+            if i == 'w':
+                mSel += -1
+                if mSel == -1:
+                    mSel = 2
+            elif i == 's':
+                mSel += 1
+                if mSel == 3:
+                    mSel = 0
+        if mSel != []:     
+            menu[mSel][0] = '[*]'
+        display()
+        i = raw_input()
+    return mSel
