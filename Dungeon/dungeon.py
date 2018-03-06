a25e36f3e61fd4e13a05f5c15be880b71d8604df
Brendan Koetting <brendank575@gmail.com>
2018-03-06 11:53:23 -0800

    Revert "thanks"
    This reverts commit feceaa878ef9117e2e72c07f9e47e08e7bc166c3.

--- Dungeon/dungeon.py
+++ Dungeon/dungeon.py
@@ -18,19 +18,6 @@ lkup = ['null','create_tile','create_chest0','create_chest1']
 fnum = 0
 floor = []
 
-heroS = 0
-heroSP = 1
-
-def heroSPFlip():
-    global heroSP
-    if heroSP == 1:
-        heroSP = 2
-    elif heroSP == 2:
-        heroSP = 1
-
-heroD = 'N'
-p_x, p_y = 2,2
-
 def read_floor():
     global floor
     floor = []
@@ -42,6 +29,8 @@ def read_floor():
                 if x != '\n':
                     row.append(x)
             floor.append(row)
+
+p_x, p_y = 2 , 2
 
 ## EMPTY ###
 
@@ -56,7 +45,11 @@ PILtile = PIL.Image.open(tilename)
 bTile = PILtile
 Tktile = PIL.ImageTk.PhotoImage(PILtile)
 def create_tile(x,y):
+<<<<<<< HEAD
     field.create_image((x*128,y*128), image = Tktile, anchor = Tkinter.NW, tags = 'sc')
+=======
+    field.create_image((x*128,y*128), image = Tktile, anchor = NW)
+>>>>>>> parent of feceaa8... thanks
 
 ## CHEST ###
 
@@ -64,18 +57,27 @@ chestname = os.path.join(directory, 'chest0.png')
 PILchest = PIL.Image.open(chestname)
 Tkchest0 = PIL.ImageTk.PhotoImage(PILchest)
 def create_chest0(x,y):
+<<<<<<< HEAD
     field.create_image((x*128,y*128), image = Tkchest0, anchor = Tkinter.NW, tags = 'sc')
+=======
+    field.create_image((x*128,y*128), image = Tkchest0, anchor = NW)
+>>>>>>> parent of feceaa8... thanks
 
 chestname = os.path.join(directory, 'chest1.png')
 PILchest = PIL.Image.open(chestname)
 Tkchest1 = PIL.ImageTk.PhotoImage(PILchest)
 def create_chest1(x,y):
+<<<<<<< HEAD
     field.create_image((x*128,y*128), image = Tkchest1, anchor = Tkinter.NW, tags = 'sc')
+=======
+    field.create_image((x*128,y*128), image = Tkchest1, anchor = NW)
+>>>>>>> parent of feceaa8... thanks
 
 ## ENEMY ###
 
 ## HERO ####
 
+<<<<<<< HEAD
 heros = os.path.join(directory, 'hero')
 Tkhero = ''
 def create_hero(direction,state):
@@ -86,6 +88,8 @@ def create_hero(direction,state):
     Tkhero = PIL.ImageTk.PhotoImage(PILhero)
     field.create_image((256,256), image = Tkhero, anchor = Tkinter.NW, tags = 'sc')
 
+=======
+>>>>>>> parent of feceaa8... thanks
 ## WALL ####
 
 ## DOOR ####
@@ -93,22 +97,19 @@ def create_hero(direction,state):
 ## LOOT ####
 
 def create_map():
-    field.delete('sc')
     for y in range(-2,3):
         for x in range(-2,3):
-            if x == 0 and y == 0:
-                create_hero(heroD,heroS)
-            else:
-                read_floor()
-                temp1 = floor[p_y+y][p_x+x]
-                temp2 = lkup[int(temp1)]
-                temp3 = temp2 + '(' + str(x+2) + ',' + str(y+2) + ')'
-                exec temp3
+            read_floor()
+            temp1 = floor[p_y+y][p_x+x]
+            temp2 = lkup[int(temp1)]
+            temp3 = temp2 + '(' + str(x+2) + ',' + str(y+2) + ')'
+            exec temp3
 
 create_map()
 
 ## STATUS DISPLAY ######################
 
+<<<<<<< HEAD
 bar = Tkinter.Canvas(root, width = 128*5, height = 128, background = '#FFFFFF')
 bar.grid(row = 0, column = 6, columnspan = 5)
 
@@ -201,6 +202,10 @@ W.grid(column = 6, row = 2, sticky = 'e')
 A = Tkinter.Button(root, width = 10, height = 4, text = 'Act')
 A.grid(column = 7, row = 2)
 
+=======
+## CONTROL PAD #########################
+
+>>>>>>> parent of feceaa8... thanks
 ## WHATEVER ############################
 
 root.mainloop()
