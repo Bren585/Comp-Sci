df1c2de048aee81371005dda4411e512f9011e01
Brendan Koetting <brendank575@gmail.com>
2018-03-06 11:49:51 -0800

    no

--- Dungeon/dungeon.py
+++ Dungeon/dungeon.py
@@ -1,9 +1,8 @@
-from Tkinter import *
+import Tkinter
 import PIL.Image, PIL.ImageTk
 import os.path
-import numpy
 
-root = Tk()
+root = Tkinter.Tk()
 directory = os.path.dirname(os.path.abspath(__file__))
 
 ## DISPLAY #####################################################################
@@ -11,7 +10,7 @@ directory = os.path.dirname(os.path.abspath(__file__))
 
 ## MAP ################################
 
-field = Canvas(root,width = 128*5, height = 128*5, background = '#000000')
+field = Tkinter.Canvas(root,width = 128*5, height = 128*5, background = '#000000')
 field.grid(row=0,column=0,rowspan = 5, columnspan = 5)
 
 lkup = ['null','create_tile','create_chest0','create_chest1']
@@ -48,6 +47,7 @@ def read_floor():
 
 def null(x,y):
     x = y
+    x == 0
 
 ## TILE ####
 
@@ -56,7 +56,7 @@ PILtile = PIL.Image.open(tilename)
 bTile = PILtile
 Tktile = PIL.ImageTk.PhotoImage(PILtile)
 def create_tile(x,y):
-    field.create_image((x*128,y*128), image = Tktile, anchor = NW, tags = 'sc')
+    field.create_image((x*128,y*128), image = Tktile, anchor = Tkinter.NW, tags = 'sc')
 
 ## CHEST ###
 
@@ -64,13 +64,13 @@ chestname = os.path.join(directory, 'chest0.png')
 PILchest = PIL.Image.open(chestname)
 Tkchest0 = PIL.ImageTk.PhotoImage(PILchest)
 def create_chest0(x,y):
-    field.create_image((x*128,y*128), image = Tkchest0, anchor = NW, tags = 'sc')
+    field.create_image((x*128,y*128), image = Tkchest0, anchor = Tkinter.NW, tags = 'sc')
 
 chestname = os.path.join(directory, 'chest1.png')
 PILchest = PIL.Image.open(chestname)
 Tkchest1 = PIL.ImageTk.PhotoImage(PILchest)
 def create_chest1(x,y):
-    field.create_image((x*128,y*128), image = Tkchest1, anchor = NW, tags = 'sc')
+    field.create_image((x*128,y*128), image = Tkchest1, anchor = Tkinter.NW, tags = 'sc')
 
 ## ENEMY ###
 
@@ -84,7 +84,7 @@ def create_hero(direction,state):
     heroname = os.path.join(heros, x)
     PILhero = PIL.Image.open(heroname)
     Tkhero = PIL.ImageTk.PhotoImage(PILhero)
-    field.create_image((256,256), image = Tkhero, anchor = NW, tags = 'sc')
+    field.create_image((256,256), image = Tkhero, anchor = Tkinter.NW, tags = 'sc')
 
 ## WALL ####
 
@@ -109,7 +109,7 @@ create_map()
 
 ## STATUS DISPLAY ######################
 
-bar = Canvas(root, width = 128*5, height = 128, background = '#FFFFFF')
+bar = Tkinter.Canvas(root, width = 128*5, height = 128, background = '#FFFFFF')
 bar.grid(row = 0, column = 6, columnspan = 5)
 
 ## CONTROL PAD #########################
@@ -186,19 +186,19 @@ def moveW():
         heroS = 0
     create_map()
 
-N = Button(root, width = 10, height = 4, text = 'North', command = moveN)
+N = Tkinter.Button(root, width = 10, height = 4, text = 'North', command = moveN)
 N.grid(column = 7, row = 1, sticky = 's')
 
-E = Button(root, width = 10, height = 4, text = 'East', command = moveE)
+E = Tkinter.Button(root, width = 10, height = 4, text = 'East', command = moveE)
 E.grid(column = 8, row = 2, sticky = 'w')
 
-S = Button(root, width = 10, height = 4, text = 'South', command = moveS)
+S = Tkinter.Button(root, width = 10, height = 4, text = 'South', command = moveS)
 S.grid(column = 7, row = 3, sticky = 'n')
 
-W = Button(root, width = 10, height = 4, text = 'West', command = moveW)
+W = Tkinter.Button(root, width = 10, height = 4, text = 'West', command = moveW)
 W.grid(column = 6, row = 2, sticky = 'e')
 
-A = Button(root, width = 10, height = 4, text = 'Act')
+A = Tkinter.Button(root, width = 10, height = 4, text = 'Act')
 A.grid(column = 7, row = 2)
 
 ## WHATEVER ############################
